52bd48741eeb2b24fab1b1c7acfc13faff7377ad
B41052021 <63893330+B41052021@users.noreply.github.com>
2020-04-18 12:44:19 +0800

    Add files via upload

---
+++ Untitled7.m
@@ -0,0 +1,9 @@
+%學號:B41052021
+%姓名:許文杰
+clear
+A=[123];
+B=[-1 3 -1];
+% dot
+c=dot(A,B)
+%cross
+D=cross(A,B)
---
+++ spower.m
@@ -0,0 +1,4 @@
+%學號:B41052021
+%姓名:許文杰
+function p=spower(x)
+p=(norm(x)^2)/length(x);
